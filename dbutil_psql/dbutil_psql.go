894dc6c9bbc17dc6561117284ee4ff581a32f1c9
Htoo Zaw Aung <25-00243@dat.com>
2024-01-10 11:59:37 +0630

    fix error at dbutil_psql.go

--- dbutil_psql/dbutil_psql.go
+++ dbutil_psql/dbutil_psql.go
@@ -1,21 +1,21 @@
 package dbutilpsql
 
 import (
-	"database/sql"
-	"fmt"
-
 	"github.com/zukigit/db-go/dbutil"
 )
 
-var db *sql.DB
-
-func DBconnect(dbsource dbutil.DBsource) {
-	fmt.Println("It's db connect")
-
-	dbsource.DBtype = "postgres"
-	err := dbutil.DBconnect(dbsource, db)
-
-	if err != nil {
-		fmt.Println(err)
-	}
+// GetDBsource creates instance DBsource object.
+//
+// Only the first three params are mandatory fields and the others are optional. You can leave empty string for optional fields. So the empty fields will be set with default values.
+func GetDBsource(DBusername string, DBpasswd string, DBname string, DBhost string, DBport string) dbutil.DBsource {
+	return dbutil.ChckDBsource(dbutil.GetDBsource(DBusername, DBpasswd, DBname, DBhost, DBport), dbutil.POSTGRESQL)
 }
+
+func DBconnect(dbsource dbutil.DBsource) error {
+	return dbutil.DBconnect(dbsource)
+}
+
+func DBselect(query string) error {
+	return dbutil.DBselect(query)
+}
+

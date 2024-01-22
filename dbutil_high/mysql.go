68cf159229fefb2ea677d1eb6e95ae467c59cb59
zuki <zuki@home>
2024-01-22 22:23:36 +0630

    added singleton

--- dbutil_high/mysql.go
+++ dbutil_high/mysql.go
@@ -2,6 +2,7 @@ package dbutil_high
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/zukigit/db-go/dbutil"
 )
@@ -11,18 +12,25 @@ type Mysql struct {
 }
 
 func DBinit_MYSQL(DBusername string, DBpasswd string, DBname string, DBhost string, DBport string) *Mysql {
+	var once sync.Once
+	var instance *Mysql
 	DBtype := "mysql"
 
 	if DBhost == "" {
 		DBhost = "localhost"
 	}
-
 	if DBport == "" {
 		DBport = "3306"
 	}
-	return &Mysql{
-		DButil: dbutil.GetInstance(DBusername, DBpasswd, DBname, DBhost, DBport, DBtype),
-	}
+
+	once.Do(
+		func() {
+			instance = new(Mysql)
+		},
+	)
+	instance.DButil = dbutil.GetInstance(DBusername, DBpasswd, DBname, DBhost, DBport, DBtype)
+
+	return instance
 }
 
 func (mysql *Mysql) DBconnect() error {

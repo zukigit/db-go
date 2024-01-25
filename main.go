8cd5adc2644b7582868559b0323593c410f7f4fe
zuki <zuki@home>
2024-01-25 22:59:43 +0630

    DBexec testing is passed

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ func main() {
 	db := dbutil.DBinit_MYSQL("root", "zabbix", "golearn", "", "")
 	db.DBconnect()
 
-	result, err := db.DBselect("select * from album")
+	result, err := db.DBselect("select * from album a where a.title = '%s'", "Blue Train")
 	if err == nil {
 		fmt.Println("result:", result)
 	}
+	afftected_rows, _ := db.DBexec("delete from album where title = '%s'", "zuki")
+	fmt.Println("afftected rows:", afftected_rows)
 }

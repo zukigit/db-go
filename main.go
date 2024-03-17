f253c0788a2fefe9d29651ecfeb9262ee7fd604f
zuki <zuki@home>
2024-03-17 20:24:37 +0700

    finished SELECT

--- main.go
+++ main.go
@@ -7,19 +7,19 @@ import (
 )
 
 func main() {
-	// DBHOST := "moon"
-	// DBUSER := "root"
-	// DBPASSWORD := "zabbix"
-	// DBNAME := "test"
-	// DBPORT := 0
-	// DBCONTIMEOUT := 1
+	DBHOST := "moon"
+	DBUSER := "root"
+	DBPASSWORD := "zabbix"
+	DBNAME := "test"
+	DBPORT := 0
+	DBCONTIMEOUT := 1
 
-	// err := db.Connect_mysql(DBHOST, DBUSER, DBPASSWORD, DBNAME, DBPORT, DBCONTIMEOUT)
-	// if err != nil {
-	// 	fmt.Printf("We get error in connecting Database. Err: %s\n", err.Error())
-	// } else {
-	// 	fmt.Println("Connection Successful!")
-	// }
+	err := db.Connect_mysql(DBHOST, DBUSER, DBPASSWORD, DBNAME, DBPORT, DBCONTIMEOUT)
+	if err != nil {
+		fmt.Printf("We get error in connecting Database. Err: %s\n", err.Error())
+	} else {
+		fmt.Println("Connection Successful!")
+	}
 
 	result, err := db.Select("select * from test;")
 	if err != nil {

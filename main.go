398503b008b4644313fe2f2f2cea36b891df4f87
zuki <zukidono@gmail.com>
2024-06-14 22:16:05 +0700

    dev is finished hopefully

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/go-sql-driver/mysql"
 	db "github.com/zukigit/db-go/dbutil"
 )
 
@@ -21,52 +22,51 @@ func main() {
 	// 	fmt.Printf("Error in connecting Database. Err: %s\n", err.Error())
 	// }
 
-	// cfg := mysql.Config{
-	// 	User:   "zabbix",
-	// 	Passwd: "zabbix",
-	// 	Net:    "tcp",
-	// 	Addr:   "",
-	// 	DBName: "zabbix",
-	// }
-	// err = db.Connect_mysql(cfg.FormatDSN())
-	// if err != nil {
-	// 	fmt.Printf("Error in connecting Database. Err: %s\n", err.Error())
-	// }
+	cfg := mysql.Config{
+		User:   "zabbix",
+		Passwd: "zabbix",
+		Net:    "tcp",
+		Addr:   "",
+		DBName: "zabbix",
+	}
+	err = db.Connect_mysql(cfg.FormatDSN())
+	if err != nil {
+		fmt.Printf("Error in connecting Database. Err: %s\n", err.Error())
+	}
 
+	// //db select
 	result, err := db.Select("select  host from hosts h where hostid = %d;", 10050)
 	if err != nil {
 		fmt.Printf("Query get failed, error: %s\n", err.Error())
 	} else {
 		fmt.Println("result:", result)
-		// for _, value := range result {
-		// 	fmt.Println(db.ResultToString(value))
-		// }
 	}
 
-	//db begin
-	// if err = db.Begin(); err != nil {
-	// 	fmt.Printf("Query get failed, error: %s\n", err.Error())
-	// }
+	// //start transaction
+	if err = db.Begin(); err != nil {
+		fmt.Printf("Query get failed, error: %s\n", err.Error())
+	}
 
-	//db execute
-	// effected_rows, err := db.Execute("insert into hosts (hostid, description) values(%d, '%s');",
-	// 	18, "")
-	// if err != nil {
-	// 	fmt.Printf("Query get failed, error: %s\n", err.Error())
-	// } else {
-	// 	fmt.Println("effected_rows:", effected_rows)
-	// }
+	// //db execute
+	effected_rows, err := db.Execute("insert into hosts (hostid, description) values(%d, '%s');",
+		7071, "")
+	if err != nil {
+		fmt.Printf("Query get failed, error: %s\n", err.Error())
+	} else {
+		fmt.Println("effected_rows:", effected_rows)
+	}
+
+	// //db commit
+	if err = db.Commit(); err != nil {
+		fmt.Printf("Query get failed, error: %s\n", err.Error())
+	}
 
 	// //db rollback
-	// if err = db.Rollback(); err != nil {
-	// 	fmt.Printf("Query get failed, error: %s\n", err.Error())
-	// }
+	if err = db.Rollback(); err != nil {
+		fmt.Printf("Query get failed, error: %s\n", err.Error())
+	}
 
-	//db commit
-	// if err = db.Commit(); err != nil {
-	// 	fmt.Printf("Query get failed, error: %s\n", err.Error())
-	// }
-
+	// //close database connection
 	if err = db.Close(); err != nil {
 		fmt.Printf("Error in closing Database. (%s)\n", err.Error())
 	}

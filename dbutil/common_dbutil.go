9eb4f4851e973fb1e8fca817a16fbcd50c99e91c
zuki <zukidono@gmail.com>
2024-06-15 11:54:38 +0700

    code clean up

--- dbutil/common_dbutil.go
+++ dbutil/common_dbutil.go
@@ -2,47 +2,6 @@ package dbutil
 
 import "errors"
 
-const (
-	//db types
-	MYSQL      = "mysql"
-	POSTGRESQL = "postgres"
-
-	//column types
-	VARCHAR            = "VARCHAR"
-	NVARCHAR           = "NVARCHAR"
-	TEXT               = "TEXT"
-	INT                = "INT"
-	UNSIGNED_INT       = "UNSIGNED INT"
-	TINYINT            = "TINYINT"
-	UNSIGNED_TINYINT   = "UNSIGNED TINYINT"
-	SMALLINT           = "SMALLINT"
-	UNSIGNED_SMALLINT  = "UNSIGNED SMALLINT"
-	MEDIUMINT          = "MEDIUMINT"
-	UNSIGNED_MEDIUMINT = "UNSIGNED MEDIUMINT"
-	BIGINT             = "BIGINT"
-	UNSIGNED_BIGINT    = "UNSIGNED BIGINT"
-	DECIMAL            = "DECIMAL"
-	BOOL               = "BOOL"
-	FLOAT              = "FLOAT"
-	DOUBLE             = "DOUBLE"
-	DATE               = "DATE"
-	DATETIME           = "DATETIME"
-	TIMESTAMP          = "TIMESTAMP"
-	TIME               = "TIME"
-	YEAR               = "YEAR"
-	CHAR               = "CHAR"
-	BINARY             = "BINARY"
-	VARBINARY          = "VARBINARY"
-	BLOB               = "BLOB"
-	MEDIUMBLOB         = "MEDIUMBLOB"
-	LONGBLOB           = "LONGBLOB"
-	JSON               = "JSON"
-	ENUM               = "ENUM"
-	SET                = "SET"
-	POINT              = "POINT"
-	GEOMETRY           = "GEOMETRY"
-)
-
 var Err_DB_NOT_INIT = errors.New("database is not initialized, use Connect_() first")
 var Err_DB_MULTIPLE_INIT = errors.New("multiple intiliaztion detected, use Close() first")
 var Err_DB_MULTIPLE_TRANSACTIONS = errors.New("doesn't not support multile transaction, you have to close one transaction using Close()")

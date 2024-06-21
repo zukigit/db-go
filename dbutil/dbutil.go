e2c6dabc65aa3b1825a1f44b9058eafce130e6c3
zuki <zukidono@gmail.com>
2024-06-22 00:44:55 +0700

    updated doc

--- dbutil/dbutil.go
+++ dbutil/dbutil.go
@@ -36,6 +36,7 @@ func Init_mysql(dbHost string, dbUser string, dbPasswd string, dbName string, db
 }
 
 // This function establishes a connection to the database and returns [dbutil.Database].
+// With that [dbutil.Database], You can use [dbutil.Database.Select] or any other utility functions.
 // It utilizes [dbutil.TEMP_DB] that must be initialized by calling [dbutil.Init_mysql_DSN] or [dbutil.Init_mysql] first.
 func Connect() (Database, error) {
 	if TEMP_DB == nil {

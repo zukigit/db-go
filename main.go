5056846f41d97909fc4555eaee60480136244eb8
zuki <zukidono@gmail.com>
2024-06-21 23:06:25 +0700

    changed to OOP

--- main.go
+++ main.go
@@ -23,15 +23,27 @@ func main() {
 		fmt.Printf("Error in connecting Database. Err: %s\n", err.Error())
 	}
 
+	if err = db.Begin(); err != nil {
+		fmt.Printf("Query get failed, error: %s\n", err.Error())
+	}
+
 	effected_rows, err := db.Execute("insert into hosts (hostid, description) values(%d, '%s');",
-		7071, "")
+		7073, "")
 	if err != nil {
 		fmt.Printf("Query get failed, error: %s\n", err.Error())
 	} else {
 		fmt.Println("effected_rows:", effected_rows)
 	}
 
-	rows, err := db.Select("select hostid from hosts where hostid = %d", 7071)
+	// if err = db.Commit(); err != nil {
+	// 	fmt.Printf("Query get failed, error: %s\n", err.Error())
+	// }
+
+	if err = db.Rollback(); err != nil {
+		fmt.Printf("Query get failed, error: %s\n", err.Error())
+	}
+
+	rows, err := db.Select("select hostid from hosts where hostid = %d", 7073)
 	if err != nil {
 		fmt.Printf("Error in select. Err: %s\n", err.Error())
 	}
@@ -42,29 +54,4 @@ func main() {
 	if err = db.Close(); err != nil {
 		fmt.Printf("Error in closing Database. (%s)\n", err.Error())
 	}
-
-	// //start transaction
-	// if err = db.Begin(); err != nil {
-	// 	fmt.Printf("Query get failed, error: %s\n", err.Error())
-	// }
-
-	// //db execute
-	// effected_rows, err := db.Execute("insert into hosts (hostid, description) values(%d, '%s');",
-	// 	7071, "")
-	// if err != nil {
-	// 	fmt.Printf("Query get failed, error: %s\n", err.Error())
-	// } else {
-	// 	fmt.Println("effected_rows:", effected_rows)
-	// }
-
-	// //db commit
-	// if err = db.Commit(); err != nil {
-	// 	fmt.Printf("Query get failed, error: %s\n", err.Error())
-	// }
-
-	// //db rollback
-	// if err = db.Rollback(); err != nil {
-	// 	fmt.Printf("Query get failed, error: %s\n", err.Error())
-	// }
-
 }
